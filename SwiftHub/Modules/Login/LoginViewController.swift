6e4ddd7583892692ec702a0e54d2fbbb4abe03fa
iFlyCai <zhangchengcai3615@126.com>
2025-03-21 03:52:45 +0800

    Update LoginViewController.swift

--- SwiftHub/Modules/Login/LoginViewController.swift
+++ SwiftHub/Modules/Login/LoginViewController.swift
@@ -25,87 +25,6 @@ enum LoginSegments: Int {
 
 class LoginViewController: ViewController {
 
-    lazy var segmentedControl: SegmentedControl = {
-        let view = SegmentedControl(sectionTitles: [])
-        view.selectedSegmentIndex = 0
-        view.snp.makeConstraints({ (make) in
-            make.width.equalTo(300)
-        })
-        return view
-    }()
-
-    // MARK: - Basic authentication
-
-    lazy var basicLoginStackView: StackView = {
-        let subviews: [UIView] = [basicLogoImageView, loginTextField, passwordTextField, basicLoginButton]
-        let view = StackView(arrangedSubviews: subviews)
-        view.spacing = inset * 2
-        return view
-    }()
-
-    lazy var basicLogoImageView: ImageView = {
-        let view = ImageView(image: R.image.image_no_result()?.template)
-        return view
-    }()
-
-    lazy var loginTextField: TextField = {
-        let view = TextField()
-        view.textAlignment = .center
-        view.keyboardType = .emailAddress
-        view.autocapitalizationType = .none
-        return view
-    }()
-
-    lazy var passwordTextField: TextField = {
-        let view = TextField()
-        view.textAlignment = .center
-        view.isSecureTextEntry = true
-        return view
-    }()
-
-    lazy var basicLoginButton: Button = {
-        let view = Button()
-        view.imageForNormal = R.image.icon_button_github()
-        view.centerTextAndImage(spacing: inset)
-        return view
-    }()
-
-    // MARK: - OAuth authentication
-
-    lazy var oAuthLoginStackView: StackView = {
-        let subviews: [UIView] = [oAuthLogoImageView, titleLabel, detailLabel, oAuthLoginButton]
-        let view = StackView(arrangedSubviews: subviews)
-        view.spacing = inset * 2
-        return view
-    }()
-
-    lazy var oAuthLogoImageView: ImageView = {
-        let view = ImageView(image: R.image.image_no_result()?.template)
-        return view
-    }()
-
-    lazy var titleLabel: Label = {
-        let view = Label()
-        view.font = view.font.withSize(22)
-        view.numberOfLines = 0
-        view.textAlignment = .center
-        return view
-    }()
-
-    lazy var detailLabel: Label = {
-        let view = Label()
-        view.font = view.font.withSize(17)
-        view.numberOfLines = 0
-        view.textAlignment = .center
-        return view
-    }()
-
-    lazy var oAuthLoginButton: Button = {
-        let view = Button()
-        view.imageForNormal = R.image.icon_button_github()
-        view.centerTextAndImage(spacing: inset)
-        return view
-    }()
 
     // MARK: - Personal Access Token authentication
 
@@ -164,23 +83,13 @@ class LoginViewController: ViewController {
     override func makeUI() {
         super.makeUI()
 
-        navigationItem.titleView = segmentedControl
-
         languageChanged.subscribe(onNext: { [weak self] () in
-            self?.segmentedControl.sectionTitles = [LoginSegments.oAuth.title, LoginSegments.personal.title, LoginSegments.basic.title]
-            // MARK: Basic
-            self?.loginTextField.placeholder = R.string.localizable.loginLoginTextFieldPlaceholder.key.localized()
-            self?.passwordTextField.placeholder = R.string.localizable.loginPasswordTextFieldPlaceholder.key.localized()
-            self?.basicLoginButton.titleForNormal = R.string.localizable.loginBasicLoginButtonTitle.key.localized()
+   
             // MARK: Personal
             self?.personalTitleLabel.text = R.string.localizable.loginPersonalTitleLabelText.key.localized()
             self?.personalDetailLabel.text = R.string.localizable.loginPersonalDetailLabelText.key.localizedFormat(Configs.App.githubScope)
             self?.personalTokenTextField.placeholder = R.string.localizable.loginPersonalTokenTextFieldPlaceholder.key.localized()
             self?.personalLoginButton.titleForNormal = R.string.localizable.loginPersonalLoginButtonTitle.key.localized()
-            // MARK: OAuth
-            self?.titleLabel.text = R.string.localizable.loginTitleLabelText.key.localized()
-            self?.detailLabel.text = R.string.localizable.loginDetailLabelText.key.localized()
-            self?.oAuthLoginButton.titleForNormal = R.string.localizable.loginOAuthloginButtonTitle.key.localized()
         }).disposed(by: rx.disposeBag)
 
         stackView.removeFromSuperview()
@@ -190,44 +99,23 @@ class LoginViewController: ViewController {
             make.centerX.equalToSuperview()
         })
 
-        titleLabel.theme.textColor = themeService.attribute { $0.text }
         personalTitleLabel.theme.textColor = themeService.attribute { $0.text }
-        detailLabel.theme.textColor = themeService.attribute { $0.textGray }
         personalDetailLabel.theme.textColor = themeService.attribute { $0.textGray }
-        basicLogoImageView.theme.tintColor = themeService.attribute { $0.text }
         personalLogoImageView.theme.tintColor = themeService.attribute { $0.text }
-        oAuthLogoImageView.theme.tintColor = themeService.attribute { $0.text }
 
-        stackView.addArrangedSubview(basicLoginStackView)
         stackView.addArrangedSubview(personalLoginStackView)
-        stackView.addArrangedSubview(oAuthLoginStackView)
     }
 
     override func bindViewModel() {
         super.bindViewModel()
         guard let viewModel = viewModel as? LoginViewModel else { return }
 
-        let segmentSelected = Observable.of(segmentedControl.segmentSelection.map { LoginSegments(rawValue: $0)! }).merge()
-        let input = LoginViewModel.Input(segmentSelection: segmentSelected.asDriverOnErrorJustComplete(),
-                                         basicLoginTrigger: basicLoginButton.rx.tap.asDriver(),
-                                         personalLoginTrigger: personalLoginButton.rx.tap.asDriver(),
-                                         oAuthLoginTrigger: oAuthLoginButton.rx.tap.asDriver())
-        let output = viewModel.transform(input: input)
-
         isLoading.asDriver().drive(onNext: { [weak self] (isLoading) in
             isLoading ? self?.startAnimating() : self?.stopAnimating()
         }).disposed(by: rx.disposeBag)
 
-        output.basicLoginButtonEnabled.drive(basicLoginButton.rx.isEnabled).disposed(by: rx.disposeBag)
-        output.personalLoginButtonEnabled.drive(personalLoginButton.rx.isEnabled).disposed(by: rx.disposeBag)
-
-        _ = loginTextField.rx.textInput <-> viewModel.login
-        _ = passwordTextField.rx.textInput <-> viewModel.password
         _ = personalTokenTextField.rx.textInput <-> viewModel.personalToken
 
-        output.hidesBasicLoginView.drive(basicLoginStackView.rx.isHidden).disposed(by: rx.disposeBag)
-        output.hidesPersonalLoginView.drive(personalLoginStackView.rx.isHidden).disposed(by: rx.disposeBag)
-        output.hidesOAuthLoginView.drive(oAuthLoginStackView.rx.isHidden).disposed(by: rx.disposeBag)
 
         error.subscribe(onNext: { [weak self] (error) in
             self?.view.makeToast(error.description, title: error.title, image: R.image.icon_toast_warning())
